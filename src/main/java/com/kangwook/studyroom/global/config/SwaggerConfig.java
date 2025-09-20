1e05c8660ad5ee53ed45f116798016d8f7aa739b
kangwook1 <rkddnr2078@naver.com>
2025-09-20 21:06:44 +0900

    [Docs] 스웨거 토큰 입력 추가 - #3

--- src/main/java/com/kangwook/studyroom/global/config/SwaggerConfig.java
+++ src/main/java/com/kangwook/studyroom/global/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.kangwook.studyroom.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +11,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
-
     @Bean
     public OpenAPI openAPI() {
+
+        SecurityScheme securityScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.APIKEY) // APIKEY 타입
+                .in(SecurityScheme.In.HEADER)     // 헤더에서 가져옴
+                .name("Authorization");           // 헤더 이름
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList("Authorization");
+
         return new OpenAPI()
-                .info(apiInfo());
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .schemaRequirement("Authorization", securityScheme);
     }
 
     private Info apiInfo() {
