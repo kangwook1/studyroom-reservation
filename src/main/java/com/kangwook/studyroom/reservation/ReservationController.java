a2b20a85bd1b3cbd8db6fa2b721a7349d6b71a2e
kangwook1 <rkddnr2078@naver.com>
2025-09-21 15:54:08 +0900

    [Docs] 스웨거 API 설명 추가 - #11

--- src/main/java/com/kangwook/studyroom/reservation/ReservationController.java
+++ src/main/java/com/kangwook/studyroom/reservation/ReservationController.java
@@ -6,34 +6,44 @@ import com.kangwook.studyroom.global.common.CommonResponse;
 import com.kangwook.studyroom.global.common.Role;
 import com.kangwook.studyroom.reservation.dto.req.ReservationReq;
 import com.kangwook.studyroom.reservation.dto.res.ReservationRes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import static com.kangwook.studyroom.global.common.StatusCode.*;
+import static com.kangwook.studyroom.global.common.StatusCode.RESERVATION_CREATED;
+import static com.kangwook.studyroom.global.common.StatusCode.RESERVATION_DELETED;
 
+
+@Tag(name = "[예약]", description = "예약 생성 및 삭제")
 @RestController
 @RequestMapping("/reservations")
 @RequiredArgsConstructor
 public class ReservationController {
     private final ReservationService reservationService;
 
+
+    @Operation(summary = "USER 전용 - 예약 생성", description = "원하는 시간대의 예약을 생성합니다. <br>"
+            + "겹치는 시간대는 예약할 수 없습니다.")
     @PostMapping
     @AuthRequired(value = Role.USER)
     public ResponseEntity<CommonResponse<ReservationRes>> createReservation(HttpServletRequest request, @RequestBody @Valid ReservationReq reservationReq) {
-        Long userId=(Long)request.getAttribute("userId");
+        Long userId = (Long) request.getAttribute("userId");
 
         return ResponseEntity.status(RESERVATION_CREATED.getStatus())
-                .body(CommonResponse.from(RESERVATION_CREATED.getMessage(),reservationService.createReservation(userId, reservationReq)));
+                .body(CommonResponse.from(RESERVATION_CREATED.getMessage(), reservationService.createReservation(userId, reservationReq)));
     }
 
+    @Operation(summary = "ADMIN, USER 전용 - 예약 취소", description = "Admin - 모든 예약 내역을 취소 가능합니다. <br>"
+            + "USER - 자신이 예약한 내역에 한해 취소할 수 있습니다.")
     @DeleteMapping("/{id}")
-    @AuthRequired(value = {Role.ADMIN, Role.USER} )
+    @AuthRequired(value = {Role.ADMIN, Role.USER})
     public ResponseEntity<CommonResponse<Object>> deleteReservation(HttpServletRequest request, @PathVariable("id") Long reservationId) {
-        Role role=(Role)request.getAttribute("role");
-        Long userId=(Long)request.getAttribute("userId");
+        Role role = (Role) request.getAttribute("role");
+        Long userId = (Long) request.getAttribute("userId");
 
         reservationService.deleteReservation(role, userId, reservationId);
 
--- src/main/java/com/kangwook/studyroom/room/RoomController.java
+++ src/main/java/com/kangwook/studyroom/room/RoomController.java
@@ -7,6 +7,8 @@ import com.kangwook.studyroom.global.common.Role;
 import com.kangwook.studyroom.room.dto.req.RoomReq;
 import com.kangwook.studyroom.room.dto.res.RoomAvailabilityRes;
 import com.kangwook.studyroom.room.dto.res.RoomRes;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,25 +20,33 @@ import java.util.List;
 import static com.kangwook.studyroom.global.common.StatusCode.ROOM_CREATED;
 import static com.kangwook.studyroom.global.common.StatusCode.ROOM_FOUND;
 
+
+@Tag(name = "[회의실]", description = "회의실 생성 및 회의실 예약 현황 조회")
 @RestController
 @RequestMapping("/rooms")
 @RequiredArgsConstructor
 public class RoomController {
     private final RoomService roomService;
 
+
+    @Operation(summary = "ADMIN 전용 - 회의실 생성", description = "회의실을 생성합니다. <br> "
+            + "중복된 이름은 사용할 수 없습니다.")
     @PostMapping
     @AuthRequired(value = Role.ADMIN)
     public ResponseEntity<CommonResponse<RoomRes>> createRoom(@RequestBody @Valid RoomReq roomReq) {
 
         return ResponseEntity.status(ROOM_CREATED.getStatus())
-                .body(CommonResponse.from(ROOM_CREATED.getMessage(),roomService.createRoom(roomReq)));
+                .body(CommonResponse.from(ROOM_CREATED.getMessage(), roomService.createRoom(roomReq)));
     }
 
+
+    @Operation(summary = "회의실 예약 현황 조회", description = "날짜에 해당하는 회의실의 예약 내역과 빈 시간대를 조회합니다. <br> "
+            + "응답 데이터의 시간은 UTC 기준입니다.")
     @GetMapping
-    public ResponseEntity<CommonResponse<List<RoomAvailabilityRes>>> createRoom(@RequestParam LocalDate date ) {
+    public ResponseEntity<CommonResponse<List<RoomAvailabilityRes>>> createRoom(@RequestParam LocalDate date) {
 
         return ResponseEntity.status(ROOM_FOUND.getStatus())
-                .body(CommonResponse.from(ROOM_FOUND.getMessage(),roomService.getRoomAvailability(date)));
+                .body(CommonResponse.from(ROOM_FOUND.getMessage(), roomService.getRoomAvailability(date)));
 
     }
 }
