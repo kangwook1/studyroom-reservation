0a84a10425726e0c93e1a78c468d72252f827886
kangwook1 <rkddnr2078@naver.com>
2025-09-21 15:21:16 +0900

    [Feat] 가용성 조회 API 구현 - #9

--- src/main/java/com/kangwook/studyroom/room/RoomController.java
+++ src/main/java/com/kangwook/studyroom/room/RoomController.java
@@ -5,16 +5,18 @@ import com.kangwook.studyroom.global.annotation.AuthRequired;
 import com.kangwook.studyroom.global.common.CommonResponse;
 import com.kangwook.studyroom.global.common.Role;
 import com.kangwook.studyroom.room.dto.req.RoomReq;
+import com.kangwook.studyroom.room.dto.res.RoomAvailabilityRes;
 import com.kangwook.studyroom.room.dto.res.RoomRes;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+import java.util.List;
 
 import static com.kangwook.studyroom.global.common.StatusCode.ROOM_CREATED;
+import static com.kangwook.studyroom.global.common.StatusCode.ROOM_FOUND;
 
 @RestController
 @RequestMapping("/rooms")
@@ -28,5 +30,13 @@ public class RoomController {
 
         return ResponseEntity.status(ROOM_CREATED.getStatus())
                 .body(CommonResponse.from(ROOM_CREATED.getMessage(),roomService.createRoom(roomReq)));
+    }
+
+    @GetMapping
+    public ResponseEntity<CommonResponse<List<RoomAvailabilityRes>>> createRoom(@RequestParam LocalDate date ) {
+
+        return ResponseEntity.status(ROOM_FOUND.getStatus())
+                .body(CommonResponse.from(ROOM_FOUND.getMessage(),roomService.getRoomAvailability(date)));
+
     }
 }
--- src/main/java/com/kangwook/studyroom/room/RoomRepository.java
+++ src/main/java/com/kangwook/studyroom/room/RoomRepository.java
@@ -1,6 +1,24 @@
 package com.kangwook.studyroom.room;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.Instant;
+import java.util.List;
 
 public interface RoomRepository extends JpaRepository<Room,Long> {
+
+    @Query("""
+    SELECT r, res
+    FROM Room r
+    LEFT JOIN Reservation res
+        ON res.roomId = r.id
+       AND res.startAt < :dayEnd
+       AND res.endAt > :dayStart
+    """)
+    List<Object[]> findRoomsWithReservations(
+            @Param("dayStart") Instant dayStart,
+            @Param("dayEnd") Instant dayEnd
+    );
 }
